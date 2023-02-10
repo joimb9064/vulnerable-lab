fb155bcb1f67e2401c6cfd91e34cec1eb0f14d23
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-02-10 15:56:36 +0100

    Update pc.sh

--- pc.sh
+++ pc.sh
@@ -1,1 +1,46 @@
+
+#Install java 8
+#!/bin/bash
+sudo apt update 
+sudo apt install openjdk-8-jdk -y
+#Samba
+echo "*********Install samba*********"
+sudo apt install samba -y
+sudo mkdir -p /samba
+sudo ufw allow samba
+echo "*******samba done**********"
+
+#install docker
+echo "***********Install docker***********"
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+echo "*********docker is done**********"
+
+#install james server
+echo "*********install james server********"
+sudo apt install bash-completion  
+curl -O https://archive.apache.org/dist/james/server/apache-james-2.3.2.tar.gz
+tar -xzf apache-james-2.3.2.tar.gz
+sudo cp -r james-2.3.2 /opt
+sudo chmod +x /opt/james-2.3.2/bin/*.sh
+echo "***********james server done **********"
+
+
+#Postfix smtp
+echo "Installing postfix smtp server"
+sudo apt update
+sudo apt install postfix
+sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.bk
+echo "***smtp done***"
+
+#Install ssh server
+sudo apt -y install openssh-server
+sudo systemctl enable --now ssh
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-arm64"
+export PATH=$PATH:$JAVA_HOME/bin
 sudo systemctl restart smbd
