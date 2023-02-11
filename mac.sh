04e007452677ec643eb0e0c1854bb2bed6b1556a
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-02-11 07:12:38 +0100

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -17,7 +17,7 @@ echo "***********Install docker***********"
 sudo apt update
 sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update
 sudo apt -y install docker-ce 
 sudo usermod -aG docker ${USER}
