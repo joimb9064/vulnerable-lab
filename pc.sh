9e22c192bdeb0f96fcefb111262b74166bc9d347
iconx <64724621+iconx2020a@users.noreply.github.com>
2023-02-16 17:21:41 +0100

    Update pc.sh

--- pc.sh
+++ pc.sh
@@ -24,7 +24,7 @@ sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-pro
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt update
-sudo apt -y install docker-ce
+sudo apt -y install docker.io
 sudo usermod -aG docker ${USER}
 echo "*********docker is done**********"
 
