cd9b6f2374346cba4a997f9a8fd5ff277c792d52
iconx <64724621+iconx2020a@users.noreply.github.com>
2024-01-05 09:26:03 +0100

    Update pc.sh

--- pc.sh
+++ pc.sh
@@ -26,7 +26,7 @@ echo "***********Install docker***********"
 
 # Add Docker's official GPG key:
 sudo apt update
-sudo apt install ca-certificates curl gnupg
+sudo apt -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
