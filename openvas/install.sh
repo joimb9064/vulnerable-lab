29375b63cc21d8c7e48ba02a472ad4ee5bcd4cb9
iconx <64724621+iconx2020a@users.noreply.github.com>
2024-01-06 10:40:15 +0100

    Update install.sh

--- openvas/install.sh
+++ openvas/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir openvas && cd openvas
-sudo usermod -aG docker $USER && su $USER
+#sudo usermod -aG docker $USER && su $USER
 curl -f -L https://greenbone.github.io/docs/latest/_static/docker-compose-22.4.yml -o docker-compose.yml
-docker-compose -f docker-compose.yml -p greenbone-community-edition up -d
+sudo docker-compose -f docker-compose.yml -p greenbone-community-edition up -d
 #open the site http://127.0.0.1:9392 on your ubuntu machine
 #Type username:admin password:admin
 #Reference https://greenbone.github.io/docs/latest/22.4/container/index.html
