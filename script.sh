747247b06ea47ef034b4b409aa0ebfc684a8dc2f
Apoorv Chaudhary <50988410+apoorv-chaudhary@users.noreply.github.com>
2024-03-12 20:46:13 +0100

    fix: updated docker and tomcat setup

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #Install java 8
 #!/bin/bash
-sudo apt update 
+sudo apt update
 sudo apt install openjdk-8-jdk -y
 #Samba
 echo "*********Install samba*********"
@@ -11,18 +11,26 @@ echo "*******samba done**********"
 
 #install docker
 echo "***********Install docker***********"
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install docker-ce -y
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker ${USER}
 echo "*********docker is done**********"
 
 #install james server
 echo "*********install james server********"
-sudo apt install bash-completion  
+sudo apt install bash-completion
 curl -O https://archive.apache.org/dist/james/server/apache-james-2.3.2.tar.gz
 tar -xzf apache-james-2.3.2.tar.gz
 sudo cp -r james-2.3.2 /opt
@@ -31,8 +39,10 @@ echo "***********james server done **********"
 
 #Tomcat
 echo "********About to install apache tomcat***"
-curl -O https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
-tar -xf apache-tomcat-10.0.27.tar.gz
+#curl -O https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
+curl -O https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
+tar -xzf apache-tomcat-10.0.27.tar.gz
+mkdir -p apache-tomcat-10.0.27/conf/Catalina/localhost/
 cp server.xml tomcat-users.xml apache-tomcat-10.0.27/conf
 cp manager.xml apache-tomcat-10.0.27/conf/Catalina/localhost/
 sudo mv apache-tomcat-10.0.27 /opt
@@ -49,7 +59,7 @@ echo "***smtp done***"
 echo "*********log4j poc************"
 mkdir ~/mylog4j
 git clone https://github.com/kozmer/log4j-shell-poc.git
-sudo mv log4j-shell-poc ~/mylog4j  
+sudo mv log4j-shell-poc ~/mylog4j
 cp poc.py ~/mylog4j/log4j-shell-poc/
 cd ~/mylog4j/log4j-shell-poc
 sudo apt install python3-pip
